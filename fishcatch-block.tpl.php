5c4189895f8db18f4a3e4ce0185c0ad0818c4829
unknown <n.brovkovich@rsit.ru>
2019-06-02 12:51:14 +0400

    listcrew

--- fishcatch-block.tpl.php
+++
@@ -1,7 +0,0 @@
-<div id="rssfeeds-pager">
-	<?php foreach ($urls as $url): ?>
-		<span>
-            <a target="_blank" href="http://<?php echo $url->url; ?>"><?php echo $url->url; ?></a><br>
-        </span>
-	<?php endforeach; ?>
-</div> 
--- fishcatch-page.tpl.php
+++
@@ -1,7 +0,0 @@
-<div id="rssfeeds-pager">
-	<?php foreach ($urls as $url): ?>
-		<span>
-            <a target="_blank" href="rss_feeds/<?php echo $url->id; ?>/items"><?php echo $url->name; ?></a><br>
-        </span>
-	<?php endforeach; ?>
-</div>
